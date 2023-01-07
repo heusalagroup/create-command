e75a4121a3ef8e9d87867079d751a1ac046dcd99
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-07 10:06:49 +0200

    Removed accidentally commited test output

--- test-command/src/test-command.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023. Heusala Group <info@heusalagroup.fi>. All rights reserved.
-
-import { main } from "./main";
-main(process.argv).then((status : number) => {
-    process.exit(status);
-}).catch((err : any) => {
-    console.error(`Error: `, err);
-    process.exit(1);
-});
